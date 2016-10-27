968222ec3a867659a3d8aefcc972ef70cc16caec
Jimzeee(@~@) <clingkhere@gmail.com>
2016-10-28 00:21:08 +0800

    build the app UI
    input textview and output textview, button

--- bigbangGRE/Resources/Resource.Designer.cs
+++ bigbangGRE/Resources/Resource.Designer.cs
@@ -61,7 +61,10 @@ namespace bigbangGRE
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int textView1 = 2131034112;
+			public const int Edittext = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int button1 = 2131034113;
 			
 			static Id()
 			{
