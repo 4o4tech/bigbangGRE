99f487428d7b1ab68681e9c732f78cdd3c54c596
Jimzeee(@~@) <clingkhere@gmail.com>
2016-10-31 23:48:28 +0800

    connect linguistic api in to app

--- bigbangGRE/MainActivity.cs
+++ bigbangGRE/MainActivity.cs
@@ -1,10 +1,19 @@
-﻿
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Linguistics;
+using Microsoft.ProjectOxford.Linguistics.Contract;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+
 using Android.App;
 using Android.Widget;
 using Android.OS;
 using System.Net;
 using System.IO;
 using System.Text;
+using Android.Util;
 
 namespace bigbangGRE
 {
@@ -16,70 +25,126 @@ namespace bigbangGRE
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
 
-            string str = "";
+           
             Button submit = FindViewById<Button>(Resource.Id.submit_button);
-            EditText editText = FindViewById<EditText>(Resource.Id.InputText);
-            EditText outputText = FindViewById<EditText>(Resource.Id.OutputText);
-            str = editText.Text;
+            EditText inputText = FindViewById<EditText>(Resource.Id.InputText);
+            TextView outputText = FindViewById<TextView>(Resource.Id.OutputText);
+            
 
             submit.Click += delegate
             {
+                string str = inputText.Text.ToString();
 
-                postStr();
+                string outputStr = running(str);
 
+                outputText.Text += str;
 
+                outputText.Text += outputStr;
 
             };
 
-
-
+            
 
         }
 
-        protected string postStr()
+
+        private static readonly LinguisticsClient Client = new LinguisticsClient("e236c06d0f214a84853dc929b1c9984a");
+
+        /// <summary>
+        /// Default jsonserializer settings
+        /// </summary>
+        private static JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings()
         {
-            string url = "https://www.microsoft.com/cognitive-services/Demo/SPLATDemo/Analyze";
-            string postStr = "language=en&analyzerIds=4fa79af1-f22c-408d-98bb-b7d7aeef7f04%2C22a6b758-420f-4745-8a3c-46835a67c0d2%2C08ea174b-bfdb-4e64-987e-602f85da7f72&text=The+Linguistic+Analysis+API+simplifies+using+to+analyze+the+GRE+sentence.%0A++++++++++++++++++++++++&__RequestVerificationToken=xJambwornt4LEHnj1kCWsNqK6hED0e52kupfJs3ohu-z5fCFyOf3obf2YHU0zOg8Odx_LYKUfBd8RDJICZrcrRKELYXjtdGGBYbiueJmiDM1";
-            var resposeStr= HttpPost(url, postStr);
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            NullValueHandling = NullValueHandling.Ignore,
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
+
+        public string  running(string inputText)
+        {
+            // List analyzers
+            Analyzer[] supportedAnalyzers = null;
+            try
+            {
+                supportedAnalyzers = ListAnalyzers();
+                var analyzersAsJson = JsonConvert.SerializeObject(supportedAnalyzers, Formatting.Indented, jsonSerializerSettings);
+                //make the analyze sentence become json formate
+                Log.Info("The json is: ", analyzersAsJson);
+                //Console.WriteLine("Supported analyzers: " + analyzersAsJson);
+            }
+            catch (Exception e)
+            {
+                Log.Error("Failed to list supported analyzers",e.ToString());
+                //Console.Error.WriteLine("Failed to list supported analyzers: " + e.ToString());
+                System.Environment.Exit(1);
+            }
+
+            // Analyze text with all available analyzers
+            var analyzeTextRequest = new AnalyzeTextRequest()
+            {
+                Language = "en",
+                AnalyzerIds = supportedAnalyzers.Select(analyzer => analyzer.Id).ToArray(),
+                Text = inputText
+            };
+
+            try
+            {
+                var analyzeTextResults = AnalyzeText(analyzeTextRequest);
+                var resultsAsJson = JsonConvert.SerializeObject(analyzeTextResults, Formatting.Indented, jsonSerializerSettings);
+
+
+                return resultsAsJson;
+                //Console.WriteLine("Analyze text results: " + resultsAsJson);
+
+
+            }
+            catch (Exception e)
+            {
+
+                Log.Error("Failed to list supported analyzers:", e.ToString());
+                //Console.Error.WriteLine("Failed to list supported analyzers: " + e.ToString());
+                return "Sorry, Error";
+                System.Environment.Exit(1);
+            }
+
             
-            return resposeStr;
+        }
+     
+
+        /// <summary>
+        /// List analyzers synchronously.
+        /// </summary>
+        /// <returns>An array of supported analyzers.</returns>
+        private static Analyzer[] ListAnalyzers()
+        {
+            try
+            {
+                return Client.ListAnalyzersAsync().Result;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Failed to gather list of analyzers", exception as ClientException);
+            }
         }
 
-
-        private string HttpPost(string Url, string postDataStr)
+        /// <summary>
+        /// Analyze text synchronously.
+        /// </summary>
+        /// <param name="request">Analyze text request.</param>
+        /// <returns>An array of analyze text result.</returns>
+        private static AnalyzeTextResult[] AnalyzeText(AnalyzeTextRequest request)
         {
-
-
-            // code form http://www.cnblogs.com/xssxss/archive/2012/07/03/2574554.html
-
-            CookieContainer cookie = new CookieContainer();
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-            request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
-            request.CookieContainer = cookie;
-            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36";
-            request.Referer = "https://www.microsoft.com/cognitive-services/en-us/linguistic-analysis-api";
-            
-
-            Stream myRequestStream = request.GetRequestStream();
-            StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("utf-8"));
-            myStreamWriter.Write(postDataStr);
-            myStreamWriter.Close();
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-            response.Cookies = cookie.GetCookies(response.ResponseUri);
-            Stream myResponseStream = response.GetResponseStream();
-            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
-            string retString = myStreamReader.ReadToEnd();
-            myStreamReader.Close();
-            myResponseStream.Close();
-
-            return retString;
+            try
+            {
+                return Client.AnalyzeTextAsync(request).Result;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Failed to analyze text", exception as ClientException);
+            }
         }
 
 
--- bigbangGRE/request.cs
+++ bigbangGRE/request.cs
@@ -1,13 +1,15 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Linguistics;
+using Microsoft.ProjectOxford.Linguistics.Contract;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
+using Android.Runtime;  
 using Android.Views;
 using Android.Widget;
 
@@ -22,5 +24,93 @@ namespace bigbangGRE
 
             // Create your application here
         }
+
+        private static readonly LinguisticsClient Client = new LinguisticsClient("e236c06d0f214a84853dc929b1c9984a");
+
+        /// <summary>
+        /// Default jsonserializer settings
+        /// </summary>
+        private static JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings()
+        {
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            NullValueHandling = NullValueHandling.Ignore,
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
+        static void Main(string[] args)
+        {
+            // List analyzers
+            Analyzer[] supportedAnalyzers = null;
+            try
+            {
+                supportedAnalyzers = ListAnalyzers();
+                var analyzersAsJson = JsonConvert.SerializeObject(supportedAnalyzers, Formatting.Indented, jsonSerializerSettings);
+                //make the analyze sentence become json formate
+                Console.WriteLine("Supported analyzers: " + analyzersAsJson);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Failed to list supported analyzers: " + e.ToString());
+                System.Environment.Exit(1);
+            }
+
+            // Analyze text with all available analyzers
+            var analyzeTextRequest = new AnalyzeTextRequest()
+            {
+                Language = "en",
+                AnalyzerIds = supportedAnalyzers.Select(analyzer => analyzer.Id).ToArray(),
+                Text = "Welcome to Microsoft Linguistic Analysis!"
+            };
+
+            try
+            {
+                var analyzeTextResults = AnalyzeText(analyzeTextRequest);
+                var resultsAsJson = JsonConvert.SerializeObject(analyzeTextResults, Formatting.Indented, jsonSerializerSettings);
+
+                Console.WriteLine("Analyze text results: " + resultsAsJson);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Failed to list supported analyzers: " + e.ToString());
+                System.Environment.Exit(1);
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// List analyzers synchronously.
+        /// </summary>
+        /// <returns>An array of supported analyzers.</returns>
+        private static Analyzer[] ListAnalyzers()
+        {
+            try
+            {
+                return Client.ListAnalyzersAsync().Result;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Failed to gather list of analyzers", exception as ClientException);
+            }
+        }
+
+        /// <summary>
+        /// Analyze text synchronously.
+        /// </summary>
+        /// <param name="request">Analyze text request.</param>
+        /// <returns>An array of analyze text result.</returns>
+        private static AnalyzeTextResult[] AnalyzeText(AnalyzeTextRequest request)
+        {
+            try
+            {
+                return Client.AnalyzeTextAsync(request).Result;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Failed to analyze text", exception as ClientException);
+            }
+        }
+
+
     }
 }
