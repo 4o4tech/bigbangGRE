61126b97591e2caeb17acc8707cdf2a1d5a16b6f
Jimzeee(@~@) <clingkhere@gmail.com>
2016-11-01 23:44:20 +0800

    add request method

---
+++ bigbangGRE/Assets/Class1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.ProjectOxford.Linguistics;
+using Microsoft.ProjectOxford.Linguistics.Contract;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+
+namespace bigbangGRE.Assets
+{
+    class LinguisticsDemo
+    {
+
+
+        
+
+    }
+}
--- bigbangGRE/MainActivity.cs
+++ bigbangGRE/MainActivity.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Json;
+using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.ProjectOxford.Linguistics;
-using Microsoft.ProjectOxford.Linguistics.Contract;
+using System.Text;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Web;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+
 
 
 using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Util;
 using System.Net;
 using System.IO;
-using System.Text;
-using Android.Util;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
 
 namespace bigbangGRE
 {
@@ -25,126 +30,137 @@ namespace bigbangGRE
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
 
-           
+
             Button submit = FindViewById<Button>(Resource.Id.submit_button);
             EditText inputText = FindViewById<EditText>(Resource.Id.InputText);
             TextView outputText = FindViewById<TextView>(Resource.Id.OutputText);
-            
 
-            submit.Click += delegate
+
+            submit.Click += async delegate
             {
                 string str = inputText.Text.ToString();
 
-                string outputStr = running(str);
+                string outputStr = "";
 
-                outputText.Text += str;
+                //outputText.Text += str;
+
+                outputStr = await MakeRequest(str);
 
                 outputText.Text += outputStr;
 
+                // Fetch the weather information asynchronously, 
+                // parse the results, then update the screen:
+                //JsonValue json = await FetchSentenceAsync(url);
+
             };
-
-            
-
         }
 
 
-        private static readonly LinguisticsClient Client = new LinguisticsClient("e236c06d0f214a84853dc929b1c9984a");
 
-        /// <summary>
-        /// Default jsonserializer settings
-        /// </summary>
-        private static JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings()
+        /*
+        // Gets weather data from the passed URL.
+        private async Task<JsonValue> FetchSentenceAsync(string url)
         {
-            DateFormatHandling = DateFormatHandling.IsoDateFormat,
-            NullValueHandling = NullValueHandling.Ignore,
-            ContractResolver = new CamelCasePropertyNamesContractResolver()
-        };
+            // Create an HTTP web request using the URL:
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
 
-
-        public string  running(string inputText)
-        {
-            // List analyzers
-            Analyzer[] supportedAnalyzers = null;
-            try
+            // Send the request to the server and wait for the response:
+            using (WebResponse response = await request.GetResponseAsync())
             {
-                supportedAnalyzers = ListAnalyzers();
-                var analyzersAsJson = JsonConvert.SerializeObject(supportedAnalyzers, Formatting.Indented, jsonSerializerSettings);
-                //make the analyze sentence become json formate
-                Log.Info("The json is: ", analyzersAsJson);
-                //Console.WriteLine("Supported analyzers: " + analyzersAsJson);
+                // Get a stream representation of the HTTP web response:
+                using (Stream stream = response.GetResponseStream())
+                {
+                    // Use this stream to build a JSON document object:
+                    JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+                    Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+                    // Return the JSON document:
+                    return jsonDoc;
+                }
             }
-            catch (Exception e)
-            {
-                Log.Error("Failed to list supported analyzers",e.ToString());
-                //Console.Error.WriteLine("Failed to list supported analyzers: " + e.ToString());
-                System.Environment.Exit(1);
-            }
-
-            // Analyze text with all available analyzers
-            var analyzeTextRequest = new AnalyzeTextRequest()
-            {
-                Language = "en",
-                AnalyzerIds = supportedAnalyzers.Select(analyzer => analyzer.Id).ToArray(),
-                Text = inputText
-            };
-
-            try
-            {
-                var analyzeTextResults = AnalyzeText(analyzeTextRequest);
-                var resultsAsJson = JsonConvert.SerializeObject(analyzeTextResults, Formatting.Indented, jsonSerializerSettings);
-
-
-                return resultsAsJson;
-                //Console.WriteLine("Analyze text results: " + resultsAsJson);
-
-
-            }
-            catch (Exception e)
-            {
-
-                Log.Error("Failed to list supported analyzers:", e.ToString());
-                //Console.Error.WriteLine("Failed to list supported analyzers: " + e.ToString());
-                return "Sorry, Error";
-                System.Environment.Exit(1);
-            }
-
-            
         }
+
+
+        // Parse the weather data, then write temperature, humidity, 
+        // conditions, and location to the screen.
+        private void ParseAndDisplay(JsonValue json)
+        {
+            
+            // Extract the array of name/value results for the field name "weatherObservation". 
+            JsonValue analyzerId = json["analyzerId"];
+            JsonValue result = json["result"];
+
+        }
+
+        */
+
      
 
-        /// <summary>
-        /// List analyzers synchronously.
-        /// </summary>
-        /// <returns>An array of supported analyzers.</returns>
-        private static Analyzer[] ListAnalyzers()
-        {
-            try
-            {
-                return Client.ListAnalyzersAsync().Result;
-            }
-            catch (Exception exception)
-            {
-                throw new Exception("Failed to gather list of analyzers", exception as ClientException);
-            }
+        static async Task<string> MakeRequest(string inputStr){
+                var client = new HttpClient();
+                var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+                // Request headers
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "f434b0a9708e47b3bf185bdbff3bd39c");
+
+                var uri = "https://api.projectoxford.ai/linguistics/v1.0/analyze?" + queryString;
+
+                HttpResponseMessage response;
+
+                var reqBody = new Body
+                {
+
+                /*
+                    
+                "language" : "en",
+		        "analyzerIds" : ["4fa79af1-f22c-408d-98bb-b7d7aeef7f04", "22a6b758-420f-4745-8a3c-46835a67c0d2"],
+		        "text" : "Hi, Macus! How are you today? Can you help me solve some GRE question?" 
+                     
+                */
+                    language = "en",
+                    analyzerIds = "",
+                    text = inputStr
+                };
+
+
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(Body));
+            MemoryStream msObj = new MemoryStream();
+            //将序列化之后的Json格式数据写入流中
+            js.WriteObject(msObj, reqBody);
+            msObj.Position = 0;
+            //从0这个位置开始读取流中的数据
+            StreamReader sr = new StreamReader(msObj, Encoding.UTF8);
+            string json = sr.ReadToEnd();
+            sr.Close();
+            msObj.Close();
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes(json);
+
+                using (var content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    response = await client.PostAsync(uri, content);
+                
+                }
+
+           return  response.ToString();
         }
 
-        /// <summary>
-        /// Analyze text synchronously.
-        /// </summary>
-        /// <param name="request">Analyze text request.</param>
-        /// <returns>An array of analyze text result.</returns>
-        private static AnalyzeTextResult[] AnalyzeText(AnalyzeTextRequest request)
+
+        public class Body
         {
-            try
-            {
-                return Client.AnalyzeTextAsync(request).Result;
-            }
-            catch (Exception exception)
-            {
-                throw new Exception("Failed to analyze text", exception as ClientException);
-            }
+            
+            public string language { get; set; }
+          
+            public string analyzerIds { get; set; }
+
+            public string text { get; set; }
+
         }
 
 
